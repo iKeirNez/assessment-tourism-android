e1e0292d5cb8bd257478c2ff1ad1f6224fd0c8ef
Keir Nellyer <keir@nellyer.co.uk>
2017-05-10 19:37:40 +0100

    Add a sick animation

--- app/src/main/java/com/keirnellyer/councilapp/SplashActivity.java
+++ app/src/main/java/com/keirnellyer/councilapp/SplashActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate_splash_icon);
+        findViewById(R.id.splash_icon).startAnimation(animation);
 
         Handler handler = new Handler();
 

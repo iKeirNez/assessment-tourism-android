1157a0d3e530928a4e21d4b2f422c3f97b1bcc68
Keir Nelyer <keir@nellyer.co.uk>
2017-06-12 21:04:01 +0100

    Set main activity to launch after animation has finished, instead of manually creating a delayed task

--- app/src/main/java/com/keirnellyer/councilapp/activity/SplashActivity.java
+++ app/src/main/java/com/keirnellyer/councilapp/activity/SplashActivity.java
@@ -2,7 +2,6 @@ package com.keirnellyer.councilapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -10,26 +9,33 @@ import android.view.animation.AnimationUtils;
 
 import com.keirnellyer.councilapp.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_icon_spinner);
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_icon_spinner);
+        animation.setAnimationListener(this);
         findViewById(R.id.splash_icon).startAnimation(animation);
+    }
 
-        Handler handler = new Handler();
+    @Override
+    public void onAnimationStart(Animation animation) {
 
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
+    }
 
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                supportFinishAfterTransition();
-            }
-        }, 2000);
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        supportFinishAfterTransition();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
